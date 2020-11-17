133bc89ce8e10f75e0678b32d5f1fe959135ba51
SinigribovS <71001180+SinigribovS@users.noreply.github.com>
2020-11-17 10:05:47 +0300

    Update 5.py

--- 5.py
+++ 5.py
@@ -1,26 +1,18 @@
 #Conform calendarului japonez, fiecare an poartă numele unui animal. Fiecare denumire sc repetă exact o dată la 12 ani. Deci, un ciclu este format din 12 ani cu următoarele denumiri de animale in această ordine sobolan, bou, tigru, iepure, dragon, şarpe, cal. oaie, maimuță, cocos, ciine, pore. Stiind că anul 2000 a fost anul Dragonului, să sescrie un algoritm care va citi de la tastatură anul (numär de patru cifre) şi va afisa denumirea lui conform calendarului japonez.
-a=int(input('Introduceti anul = '))
-if a%12==1:
-    print('Sobolan')
-if a%12==2:
-    print('Bou')
-if a%12==3:
-    print('Tigru')
-if a%12==4:
-    print('Iepure')
-if a%12==5:
-    print('Dragon')
-if a%12==6:
-    print('Sarpe')
-if a%12==7:
-    print('Cal')
-if a%12==8:
-    print('Oaie')
-if a%12==9:
-    print('Maimuta')
-if a%12==10:
-    print('Cocos')
-if a%12==11:
-    print('Caine')
-if a%12==12:
-    print('Porc')
+n=int(input('Introduceti anul(numar de patru cifre):'))
+
+h=input('Inainte sau dupa Hristos(introduceti "i" sau "d")')
+
+if h=='d':
+    if n>=1996:
+        l=['sobolan','bou','tigru','iepure','dragon','sarpe','cal','oaie','maimuta','cocos','caine','porc']
+        i=(n-1996)%12
+        print(l[i])
+    elif n<1996 and n>=0:
+        l=['porc','caine','cocos','maimuta','oaie','cal','sarpe','dragon','iepure','tigru','bou','sobolan']
+        i=(1995-n)%12
+        print(l[i])
+elif h=='i':
+    l=['maimuta','oaie','cal','sarpe','dragon','iepure','tigru','bou','sobolan','porc','caine','cocos']
+    i=n%12
+    print(l[i])
